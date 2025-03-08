01905662e5561706925e90c50677e15c6eaa3dc7
Jasleen Kaur <jasleen2357@gmail.com>
2025-03-08 15:20:46 -0800

    Added React Router Support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { DummyPage } from "./pages/DummyPage";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+   <Routes>
+      <Route path="/dummy" element={<DummyPage />} />
+   </Routes>
   );
 }
 
