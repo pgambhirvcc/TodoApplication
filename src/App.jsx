5654cd67bcf6fcd990bc7c39713a603d64227cfd
Jasleen Kaur <jasleen2357@gmail.com>
2025-03-08 16:16:10 -0800

    Login page feature done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { DummyPage } from "./pages/DummyPage";
 import { SignupPage } from "./pages/SignupPage";
+import { LoginPage } from "./pages/LoginPage";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
    <Routes>
       <Route path="/dummy" element={<DummyPage />} />
       <Route path="/signup" element={<SignupPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<HomePage />} />
    </Routes>
   );
 }
