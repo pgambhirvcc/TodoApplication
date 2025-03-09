3604d9494e5d85f71855e0a5cf8b7b1595733902
Jasleen Kaur <jasleen2357@gmail.com>
2025-03-08 16:06:27 -0800

    Signup UI Feature done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { DummyPage } from "./pages/DummyPage";
+import { SignupPage } from "./pages/SignupPage";
 
 function App() {
   return (
    <Routes>
       <Route path="/dummy" element={<DummyPage />} />
+      <Route path="/signup" element={<SignupPage />} />
    </Routes>
   );
 }
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Signup } from '../components/authentication/Signup'
+
+export const SignupPage = () => {
+  return (
+    <div>
+        <Signup />
+    </div>
+  )
+}
